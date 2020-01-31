781f942fb0e603f6410ff586eb5656a02739be13
CedricDeMits <cedric.demits@efeso.com>
2020-01-31 22:21:13 +0100

    input validation sorta

--- main.rb
+++ main.rb
@@ -4,9 +4,10 @@ $max_coords = nil
 
 def execute
   begin
+    puts 'Type exit to quit'
     max_grid
     puts '=====MARS=ROVER=TIME==='
-    until @input == 'exit'
+    until @input == 'exit' # Whatever, I need a loop anyways
       create_rover
       move_rover
       report_rover
@@ -18,26 +19,27 @@ def execute
 end
 
 def max_grid
-  puts 'Type exit to quit'
   puts 'Please enter max grid size in format "X Y"'
-  @input = gets.chomp.split(' ')
-  $max_coords = Coordinate.new(x: @input[0].to_i, y: @input[1].to_i)
+  get_input
+  coordinates = @input&.split(' ')
+  $max_coords = Coordinate.new(x: coordinates[0].to_i, y: coordinates[1].to_i) if coordinates
 end
 
 def create_rover
   puts 'Please enter new rover starting position and orientation'
   puts 'Format X Y Orientation'
-  @input = gets.chomp.split(' ')
-  input_coordinates = @input
+  get_input
+  input_coordinates = @input&.split(' ')
   puts 'Please enter movement instructions'
   puts 'L = Turn left | R = Turn right | M = Move forward'
-  @input = gets.chomp.split('')
-  input_instructions = @input
+  get_input
+  input_instructions = @input&.split('')
+
   @rover = Rover.new(
     coordinates: { x: input_coordinates[0].to_i, y: input_coordinates[1].to_i },
     orientation: input_coordinates[2],
     instructions: input_instructions
-  )
+  ) if input_coordinates && input_instructions
 end
 
 def move_rover
@@ -49,8 +51,13 @@ def report_rover
   puts @rover.to_s
 end
 
+def get_input
+  @input = gets&.chomp
+  raise Interrupt if @input == 'exit' || @input.nil?
+end
+
 begin
   execute
-rescue StandardError, Interrupt
-  puts "(ﾉಠдಠ)ﾉ︵┻━┻"
+rescue Interrupt
+  puts "(ﾉಠдಠ)ﾉ︵┻━┻ Fine, get out!"
 end
